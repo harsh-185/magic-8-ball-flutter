03b73c0dd4c85ef7934f01c4ff8785b86f533643
harsh-185 <harsh.maheshwari180504@gmail.com>
2023-05-13 00:41:32 +0530

    com

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
+void main() {runApp (EightBall());}
+
+class EightBall extends StatefulWidget {
+  
+  @override
+  State<EightBall> createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset('images/ball1.png')
     );
+  }
+}
