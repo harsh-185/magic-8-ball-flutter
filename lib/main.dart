2f9321884268d555a3fd41fc3501c82625beb0f1
harsh-185 <harsh.maheshwari180504@gmail.com>
2023-05-13 01:59:46 +0530

    com

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {runApp (EightBall());}
+void main() {runApp (
+    MaterialApp(
+    home: BallPage(
+    )
+  )
+);
+}
 
 class EightBall extends StatefulWidget {
   
@@ -9,11 +16,39 @@ class EightBall extends StatefulWidget {
 }
 
 class _EightBallState extends State<EightBall> {
+
+  int num = 1;
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset('images/ball1.png')
+    return Center(
+      child: TextButton(
+        onPressed:() {
+          setState(() {
+            num = randomize();
+          });
+        },
+          child: Image.asset('images/ball$num.png'))
     );
   }
+}
+
+class BallPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Ask me Anything'),
+        ),
+        body: EightBall()
+    );
+  }
+}
+
+int randomize() {
+  var num = Random().nextInt(5) + 1;
+  return num;
 }
